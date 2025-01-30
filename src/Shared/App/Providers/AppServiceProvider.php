f7f1caf7089881dbbe3f848f82e3147b5131ee07
Gianfranco Rocco <gianfrancorocco.business@gmail.com>
2025-01-30 14:04:13 -0300

    build: telescope

--- src/Shared/App/Providers/AppServiceProvider.php
+++ src/Shared/App/Providers/AppServiceProvider.php
@@ -23,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
